4756ce30fecf35a52bf8411baa856e9f9421205f
Ivaynn <ivaynn@gmail.com>
2025-08-17 14:06:23 +0100

    improved "air" block tag for blocks that spells can go through

---
+++ tools/blocks.py
@@ -0,0 +1,22 @@
+
+from pathlib import Path
+from spells import read_json, save_json
+
+
+def main() -> None:
+    datapack_root = Path('../datapack')
+
+    # https://github.com/PrismarineJS/minecraft-data/blob/master/data/pc/1.21.6/blocks.json
+    data: list[dict] = read_json('blocks.json')
+
+    block_tag = {'values': []}
+
+    for block in data:
+        if block.get('boundingBox',None) == 'empty':
+            block_tag['values'].append('minecraft:'+block['name'])
+    
+    save_json(block_tag, datapack_root / 'data/spellcrafter/tags/block/air.json')
+
+
+if __name__ == '__main__':
+    main()
