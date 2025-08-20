845cd1f4a667b394e45afa1398b2288653c8c15d
Ivaynn <ivaynn@gmail.com>
2025-08-20 17:46:53 +0100

    arena - fixes

---
+++ tools/sc-server.py
@@ -0,0 +1,10 @@
+
+import shutil
+from pathlib import Path
+
+path_target = Path('../../sc-server')
+path_datapack = Path('../datapack')
+path_arena = Path('../arena')
+
+shutil.copytree(path_datapack / 'data/spellcrafter', path_target / 'data/spellcrafter', dirs_exist_ok=True)
+shutil.copytree(path_arena / 'data/scarena', path_target / 'data/scarena', dirs_exist_ok=True)
