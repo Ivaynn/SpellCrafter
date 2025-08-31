b718116feb40e4f6385d5817798e6d13ebe192f9
Ivaynn <ivaynn@gmail.com>
2025-08-31 10:58:12 +0100

    update version numbers

--- tools/build.py
+++ tools/build.py
@@ -1,7 +1,18 @@
 import shutil
+import json
+from pathlib import Path
 
 version = '0.2.0'
 
+path = Path('../datapack/pack.mcmeta')
+mcmeta = json.loads(path.read_text(encoding='utf-8'))
+mcmeta['pack']['description'] = f'Ivaynn\'s SpellCrafter {version}'
+path.write_text(json.dumps(mcmeta, indent=4, ensure_ascii=False), encoding='utf-8')
+
+path = Path('../resources/pack.mcmeta')
+mcmeta = json.loads(path.read_text(encoding='utf-8'))
+mcmeta['pack']['description'] = f'Resource Pack for Ivaynn\'s SpellCrafter {version}'
+path.write_text(json.dumps(mcmeta, indent=4, ensure_ascii=False), encoding='utf-8')
+
 shutil.make_archive(f'SpellCrafter-{version}', 'zip', '../datapack')
 shutil.make_archive(f'SpellCrafter-{version}-ResourcePack', 'zip', '../resources')
-shutil.make_archive(f'SpellCrafter-{version}-Arena', 'zip', '../arena')
