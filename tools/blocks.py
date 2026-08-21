c362083b7b73966f7af8e228b372ed5b855f20e6
Ivaynn <ivaynn@gmail.com>
2026-08-22 00:11:25 +0100

    improve air block tag generator

--- tools/blocks.py
+++ tools/blocks.py
@@ -6,15 +6,29 @@ from utils import read_json, save_json
 def main() -> None:
     datapack_root = Path('../datapack')
 
-    # https://github.com/PrismarineJS/minecraft-data/blob/master/data/pc/1.21.6/blocks.json
-    data: list[dict] = read_json('blocks.json')
-
+    # https://github.com/PrismarineJS/minecraft-data/blob/master/data/pc/26.1/blocks.json
+    block_data: list[dict] = read_json('blocks.json')
+    shape_data: dict[str,dict] = read_json('blockCollisionShapes.json')
 
     # Blocks without bbox to be considered "air" for raycasting
     air_block_tag = {'values': []}
-    for block in data:
+    for block in block_data:
         if block.get('boundingBox', None) == 'empty':
             air_block_tag['values'].append('minecraft:'+ block['name'])
+            continue
+
+        shape_ids = shape_data['blocks'][block['name']]
+        shape_ids = [shape_ids] if isinstance(shape_ids, int) else shape_ids
+
+        volume = 0
+
+        for shape_id in shape_ids:
+            for bbox in shape_data['shapes'][str(shape_id)]:
+                volume += (bbox[3] * bbox[4] * bbox[5])
+
+        if volume < 0.25:
+            air_block_tag['values'].append('minecraft:'+ block['name'])
+
     save_json(air_block_tag, datapack_root / 'data/spellcrafter/tags/block/air.json')
 
 
@@ -22,7 +36,7 @@ def main() -> None:
     explode_small = {'values': []}
     explode_large = {'values': ["#spellcrafter:explode_small"]}
 
-    for block in data:
+    for block in block_data:
         if not block.get('diggable', False):
             continue
         resistance = block.get('resistance', 3_600_000)
